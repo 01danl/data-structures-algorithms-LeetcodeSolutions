25795bafb81bc7128766da0b02a8cbdba7c2180f
Daniyal <bisenovdanial@gmail.com>
2025-05-17 01:13:03 +0500

    Create a mergSort LL

---
+++ Algorithms/mergeSort_LinkedList.py
@@ -0,0 +1,37 @@
+class ListNode():
+    def __init__(self, val):
+        self.val = val 
+        self.next = None 
+
+def merge_sort(head):
+    if head is None or not head.next:
+        return head
+
+    slow, fast = head, head.next # slow (0) fast (1)
+
+    while fast and fast.next: #divide
+        slow = slow.next
+        fast = fast.next.next
+
+    mid = slow.next
+    slow.next = None
+
+    merge_sort(head)
+    merge_sort(mid)
+
+    dummy = ListNode()
+    curr = dummy
+
+    while left and right:
+        if left.val < right.val:
+            curr.next = left #create value new nodes - > - >
+            left = left.next
+        else:
+            curr.next = right
+            right = right.next
+        curr = curr.next 
+    
+    curr.next = left or right 
+    return dummy.next 
+
+
